24147c195157aade6b1e1979442a4ea11dff682e
swati datir <swatibhagwa@gmail.com>
2021-12-18 10:07:21 +0530

    While loop using Break

---
+++ While loop.py
@@ -0,0 +1,16 @@
+# i = 0
+# while i < len([1,2,3]):
+#     print(i)
+#     i += 1
+
+# we can do it by using listname
+# my_list = [1,2,3]
+# i=0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+# Using Break
+while True:
+    Response = input("Say something:  ")
+    break
