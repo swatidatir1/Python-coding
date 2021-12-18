47a778a57dda9b9d3ffdaed8de93060b50202fc5
swati datir <swatibhagwa@gmail.com>
2021-12-18 10:44:57 +0530

    X-mas Tree

---
+++ GUI.py
@@ -0,0 +1,15 @@
+Picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+for item in Picture:
+    for pixel in item:
+        if (pixel == 0):
+            print(' ', end='')
+        else:
+            print("*", end='')
+    print('')
