18e1deadef50e2d65f0925df9d180f44ae99bb9e
swati datir <swatibhagwa@gmail.com>
2021-12-18 11:36:50 +0530

    Default parameter concept

---
+++ Method.py
@@ -0,0 +1,11 @@
+# parameters
+# default parameter
+def say_hello(name='Sayujya', emoji=":D"):
+    print(f'Hello {name}  {emoji}')
+
+
+# argument
+say_hello("swati", ":P ")
+say_hello("Parth", ":P")
+say_hello(":)", "Suyog")
+say_hello()
